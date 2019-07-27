d89faacc9ddca2cb0f5fb9e45b309975bd360e60
Lom812 <galaktionov@galaktionov.ru>
2019-07-27 13:40:50 +0300

    ProgrammingAssignment2 - created cachematrix.R script for the Assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## ProgrammingAssignment2, R Programming, Week 3, Data Science
+## Data Science course
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Matrix inversion and cashing function
+makeCacheMatrix <- function(matrix_in = matrix()) {
+        matrix_m <- NULL
+        set_mx <- function(matrix_set) {
+                matrix_in <<- matrix_set
+                matrix_m <<- NULL
+        }
+        get_mx <- function() matrix_in
+        set_mx_inv <- function(mxsolve) {
+                matrix_m <<- solve(mxsolve)
+        }
+        get_mx_inv <- function() matrix_m
+        list(setmx = set_mx,
+             getmx = get_mx,
+             setmxinv = set_mx_inv,
+             getmxinv = get_mx_inv)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Matrix inversion with cashe use condition function
+cacheSolve <- function(matrix_in, ...) {
+        matrix_m <- matrix_in$getmxinv()
+        #print(matrix_m)
+        if(!is.null(matrix_m)) {
+                message("Getting cached data")
+                return(matrix_m)
+        }
+        data <- matrix_in$getmx()
+        matrix_m <- solve(data, ...)
+        matrix_in$setmxinv(matrix_m)
+        matrix_m
 }
